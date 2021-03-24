8ed80deecdfac655d8ff629024ccc59e07725963
Dani <zsikla.dani@gmail.com>
2021-03-24 22:09:35 +0100

    filefeltöltés elkezdés backedn

---
+++ src/main/java/com/example/demo/storage/StorageProperties.java
@@ -0,0 +1,36 @@
+package com.example.demo.storage;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ConfigurationProperties("storage")
+public class StorageProperties {
+
+	/**
+	 * Folder location for storing files
+	 */
+	private String location = "upload-dir";
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+
+}
+
+
